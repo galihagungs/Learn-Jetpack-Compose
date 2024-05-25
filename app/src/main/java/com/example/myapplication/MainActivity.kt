1e210242ae95295ce8651de3c4efe842cc711832
Galih Agung S <galihagung223@gmail.com>
2024-05-25 14:57:57 +0700

    Udemy Section 6, Vid 15

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -54,6 +58,21 @@ fun CreateBizCard(){
                 defaultElevation = 4.dp
             )
         ) {
+
+            Surface (modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                shadowElevation = 4.dp,
+                tonalElevation = 4.dp,
+                color = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f)
+            ){
+                Image(painter = painterResource(id = R.drawable.senna),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop)
+            }
 
         }
 
